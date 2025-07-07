8843305e5942ea062e71532ae224c3180e3cf55d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-07 15:21:15 +0500

    "Unable to create a 'DbContext' of type 'ApplicationDbContext'. The exception 'The entity type 'Book' requires a primary key to be defined. If you intended to use a keyless entity type, call 'HasNoKey' in 'OnModelCreating'. For more information on keyless entity types, see https://go.microsoft.com/fwlink/?linkid=2141943.' was thrown while attempting to create an instance. For the different patterns supported at design time, see https://go.microsoft.com/fwlink/?linkid=851728" issue resolved.

--- EFCore_Models/Models/Book.cs
+++ EFCore_Models/Models/Book.cs
@@ -1,7 +1,10 @@
-﻿namespace EFCore_Models.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCore_Models.Models
 {
 	public class Book
 	{
+		[Key]
 		public int IDBook { get; set; }
 		public string Title { get; set; }
 		public string ISBN { get; set; }
