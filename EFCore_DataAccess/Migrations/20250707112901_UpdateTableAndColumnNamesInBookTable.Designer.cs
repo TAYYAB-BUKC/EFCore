69a79c85ecfe52e1f03979c848d74016aaa755ff
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-07 16:29:59 +0500

    Migration added for updating table and column names in Book table.

--- EFCore_DataAccess/Migrations/20250707112901_UpdateTableAndColumnNamesInBookTable.Designer.cs
+++ EFCore_DataAccess/Migrations/20250707112901_UpdateTableAndColumnNamesInBookTable.Designer.cs
@@ -3,6 +3,7 @@ using EFCore_DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFCore_DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250707112901_UpdateTableAndColumnNamesInBookTable")]
+    partial class UpdateTableAndColumnNamesInBookTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EFCore_DataAccess/Migrations/20250707112901_UpdateTableAndColumnNamesInBookTable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTableAndColumnNamesInBookTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Books",
+                table: "Books");
+
+            migrationBuilder.RenameTable(
+                name: "Books",
+                newName: "TBL_Books");
+
+            migrationBuilder.RenameColumn(
+                name: "ISBN",
+                table: "TBL_Books",
+                newName: "Book_ISBN");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TBL_Books",
+                table: "TBL_Books",
+                column: "IDBook");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TBL_Books",
+                table: "TBL_Books");
+
+            migrationBuilder.RenameTable(
+                name: "TBL_Books",
+                newName: "Books");
+
+            migrationBuilder.RenameColumn(
+                name: "Book_ISBN",
+                table: "Books",
+                newName: "ISBN");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Books",
+                table: "Books",
+                column: "IDBook");
+        }
+    }
+}
