a304fc19e63f0d9ad27e767107467850e9ce385c
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-07 15:10:59 +0500

    ApplicationDbContext initial implementation added.

---
+++ EFCore_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using EFCore_Models.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_DataAccess.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public DbSet<Book> Books { get; set; }
+	}
+}
