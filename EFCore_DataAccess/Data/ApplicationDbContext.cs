4659d9b99e7e78d1271afbdcfb1e4968b40c4cc0
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-07 16:21:38 +0500

    Seed data for Book Table using OnModelCreating.

--- EFCore_DataAccess/Data/ApplicationDbContext.cs
+++ EFCore_DataAccess/Data/ApplicationDbContext.cs
@@ -17,6 +17,47 @@ namespace EFCore_DataAccess.Data
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
 			modelBuilder.Entity<Book>().Property(b => b.Price).HasPrecision(10, 6);
+
+			modelBuilder.Entity<Book>().HasData(
+				new Book
+				{
+					IDBook = 1,
+					ISBN = "123456",
+					Title = "Spider Without Duty",
+					Price = 10.99m
+				},
+				new Book
+				{
+					IDBook = 2,
+					ISBN = "123456456",
+					Title = "Fortune Of Time",
+					Price = 11.99m
+				});
+
+			var bookList = new Book[]
+			{
+				new Book
+				{
+					IDBook = 3,
+					ISBN = "123456789",
+					Title = "Fake Sunday",
+					Price = 20.99m
+				},
+				new Book
+				{
+					IDBook = 4,
+					ISBN = "0123456789",
+					Title = "Cookie Jar",
+					Price = 25.99m
+				},
+				new Book
+				{
+					IDBook = 5,
+					ISBN = "1234567890",
+					Title = "Cloudy Forest",
+					Price = 40.99m
+				}
+			};
 		}
 	}
 }
