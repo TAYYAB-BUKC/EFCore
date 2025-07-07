a89a30d0e0286fa50e885d5df88644cb3c849b04
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-07 16:57:20 +0500

    SubCategory model added in Models Project.

---
+++ EFCore_Models/Models/SubCategory.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCore_Models.Models
+{
+	public class SubCategory
+	{
+		[Key]
+		public int SubCategory_Id { get; set; }
+		[Required]
+		[MaxLength(50)]
+		public string Name { get; set; }
+	}
+}
