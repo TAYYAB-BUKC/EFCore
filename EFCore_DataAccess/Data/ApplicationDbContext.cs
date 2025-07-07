d2e5b3823013cdb375271340a806e46c8021f75a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-07 16:55:11 +0500

    Publisher model added in ApplicationDbContext.

--- EFCore_DataAccess/Data/ApplicationDbContext.cs
+++ EFCore_DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,8 @@ namespace EFCore_DataAccess.Data
 
 		public DbSet<Author> Authors { get; set; }
 
+		public DbSet<Publisher> Publishers { get; set; }
+
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseSqlServer("Server=DESKTOP-MTIIACB\\SQLEXPRESS;Database=EFCore;TrustServerCertificate=True;Trusted_Connection=True;");
