13dfd85b5594f95dc8cc2a8c547ffeef49fe0fd2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-07 16:28:34 +0500

    Table and Column DataAnnotations applied on Book Model.

--- EFCore_Models/Models/Book.cs
+++ EFCore_Models/Models/Book.cs
@@ -1,12 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCore_Models.Models
 {
+	[Table("TBL_Books")]
 	public class Book
 	{
 		[Key]
 		public int IDBook { get; set; }
 		public string Title { get; set; }
+		[Column("Book_ISBN")]
 		public string ISBN { get; set; }
 		public decimal Price { get; set; }
 	}
