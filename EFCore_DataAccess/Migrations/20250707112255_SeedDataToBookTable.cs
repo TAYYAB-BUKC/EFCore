e7f4f3a9d301d5c44f433819f158a3e5f7120880
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-07 16:24:01 +0500

    Migration added for seeding data to Book table.

---
+++ EFCore_DataAccess/Migrations/20250707112255_SeedDataToBookTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EFCore_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataToBookTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "IDBook", "ISBN", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "123456", 10.99m, "Spider Without Duty" },
+                    { 2, "123456456", 11.99m, "Fortune Of Time" },
+                    { 3, "123456789", 20.99m, "Fake Sunday" },
+                    { 4, "0123456789", 25.99m, "Cookie Jar" },
+                    { 5, "1234567890", 40.99m, "Cloudy Forest" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "IDBook",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "IDBook",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "IDBook",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "IDBook",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "IDBook",
+                keyValue: 5);
+        }
+    }
+}
