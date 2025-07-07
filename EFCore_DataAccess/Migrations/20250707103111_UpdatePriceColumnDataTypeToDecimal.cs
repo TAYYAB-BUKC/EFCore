19d81343cb086e6287e35b2de9e1a5a6e9b51735
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-07 15:33:25 +0500

    Migration added for Price datatype change.

---
+++ EFCore_DataAccess/Migrations/20250707103111_UpdatePriceColumnDataTypeToDecimal.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePriceColumnDataTypeToDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Books",
+                type: "decimal(10,6)",
+                precision: 10,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Books",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,6)",
+                oldPrecision: 10,
+                oldScale: 6);
+        }
+    }
+}
