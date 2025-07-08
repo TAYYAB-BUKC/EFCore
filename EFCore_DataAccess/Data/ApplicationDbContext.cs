520d2268e9f6516ef800b4dd84b17cf6afed27e7
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-08 22:57:52 +0500

    BookDetail model added in ApplicationDbContext.

--- EFCore_DataAccess/Data/ApplicationDbContext.cs
+++ EFCore_DataAccess/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ namespace EFCore_DataAccess.Data
 
 		public DbSet<SubCategory> SubCategories { get; set; }
 
+		public DbSet<BookDetail> BookDetails { get; set; }
+
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseSqlServer("Server=DESKTOP-MTIIACB\\SQLEXPRESS;Database=EFCore;TrustServerCertificate=True;Trusted_Connection=True;");
