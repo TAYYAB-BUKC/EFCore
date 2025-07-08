11210b3b0ea8a99c7bb3ec7e034bde83be97b37e
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-08 22:57:18 +0500

    BookDetail model added in Models Project.

---
+++ EFCore_Models/Models/BookDetail.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCore_Models.Models
+{
+	public class BookDetail
+	{
+		[Key]
+		public int BookDetail_Id { get; set; }
+		[Required]
+		public int NumberOfChapters { get; set; }
+		public int NumberOfPages { get; set; }
+		public string Weight { get; set; }
+	}
+}
