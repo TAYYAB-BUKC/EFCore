fea797718c89585fe3daf012972d58ac9338ac76
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-09 20:53:42 +0500

    GetAllBooks implemented in Console Project.

--- EFCore_Console/Program.cs
+++ EFCore_Console/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EFCore_DataAccess.Data;
+using EFCore_Models.Models;
 using Microsoft.EntityFrameworkCore;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 using (ApplicationDbContext dbContext = new())
 {
@@ -13,4 +14,17 @@ using (ApplicationDbContext dbContext = new())
 	{
 		await dbContext.Database.MigrateAsync();
 	}
+
+	var books = await GetAllBooks(dbContext);
+	foreach (var book in books)
+	{
+		Console.WriteLine($"{book.Title} - {book.ISBN}");
+	}
+}
+
+Console.ReadKey();
+
+async Task<List<Book>> GetAllBooks(ApplicationDbContext dbContext)
+{
+	return await dbContext.Books.ToListAsync();
 }
