66c9a4cff777221c91e7248dc7805e5259bb689a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-09 09:36:41 +0500

    Migration added for Many to Many relation between Fluent_Book and Fluent_Author.

---
+++ EFCore_DataAccess/Migrations/20250709043547_ManyToManyRelationBetweenFluentBookAndFluentAuthorAdded.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ManyToManyRelationBetweenFluentBookAndFluentAuthorAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fluent_AuthorFluent_Book",
+                columns: table => new
+                {
+                    AuthorsAuthor_Id = table.Column<int>(type: "int", nullable: false),
+                    BooksIDBook = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fluent_AuthorFluent_Book", x => new { x.AuthorsAuthor_Id, x.BooksIDBook });
+                    table.ForeignKey(
+                        name: "FK_Fluent_AuthorFluent_Book_Fluent_Authors_AuthorsAuthor_Id",
+                        column: x => x.AuthorsAuthor_Id,
+                        principalTable: "Fluent_Authors",
+                        principalColumn: "Author_Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Fluent_AuthorFluent_Book_TBL_FluentBooks_BooksIDBook",
+                        column: x => x.BooksIDBook,
+                        principalTable: "TBL_FluentBooks",
+                        principalColumn: "IDBook",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fluent_AuthorFluent_Book_BooksIDBook",
+                table: "Fluent_AuthorFluent_Book",
+                column: "BooksIDBook");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fluent_AuthorFluent_Book");
+        }
+    }
+}
