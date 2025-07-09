98a1506c256bbedafa89860729047ab97e431061
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-09 08:04:49 +0500

    Migration added for Many to Many relation between Book and Author with Mapping Table.

---
+++ EFCore_DataAccess/Migrations/20250709030422_ManyToManyRelationAddedWithMappingTableBetweenBookAndAuthor.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ManyToManyRelationAddedWithMappingTableBetweenBookAndAuthor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorBook");
+
+            migrationBuilder.CreateTable(
+                name: "BookAuthorMapping",
+                columns: table => new
+                {
+                    Book_Id = table.Column<int>(type: "int", nullable: false),
+                    Author_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookAuthorMapping", x => new { x.Author_Id, x.Book_Id });
+                    table.ForeignKey(
+                        name: "FK_BookAuthorMapping_Authors_Author_Id",
+                        column: x => x.Author_Id,
+                        principalTable: "Authors",
+                        principalColumn: "Author_Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookAuthorMapping_TBL_Books_Book_Id",
+                        column: x => x.Book_Id,
+                        principalTable: "TBL_Books",
+                        principalColumn: "IDBook",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookAuthorMapping_Book_Id",
+                table: "BookAuthorMapping",
+                column: "Book_Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookAuthorMapping");
+
+            migrationBuilder.CreateTable(
+                name: "AuthorBook",
+                columns: table => new
+                {
+                    AuthorsAuthor_Id = table.Column<int>(type: "int", nullable: false),
+                    BooksIDBook = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorBook", x => new { x.AuthorsAuthor_Id, x.BooksIDBook });
+                    table.ForeignKey(
+                        name: "FK_AuthorBook_Authors_AuthorsAuthor_Id",
+                        column: x => x.AuthorsAuthor_Id,
+                        principalTable: "Authors",
+                        principalColumn: "Author_Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorBook_TBL_Books_BooksIDBook",
+                        column: x => x.BooksIDBook,
+                        principalTable: "TBL_Books",
+                        principalColumn: "IDBook",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorBook_BooksIDBook",
+                table: "AuthorBook",
+                column: "BooksIDBook");
+        }
+    }
+}
