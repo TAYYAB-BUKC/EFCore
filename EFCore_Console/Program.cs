97afa71fc714bb0aebdf2d0a9dcc14723712d534
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-09 20:47:23 +0500

    Database basic necessary helper methods implemented

--- EFCore_Console/Program.cs
+++ EFCore_Console/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using EFCore_DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+using (ApplicationDbContext dbContext = new())
+{
+	// Create database if doesnot exist
+	await dbContext.Database.EnsureCreatedAsync();
+
+	if((await dbContext.Database.GetPendingMigrationsAsync()).Count() > 0)
+	{
+		await dbContext.Database.MigrateAsync();
+	}
+}
