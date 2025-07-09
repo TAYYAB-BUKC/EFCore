bc78bc735e9a4303c410e41ae8718f7aaf3e26fc
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-09 07:27:54 +0500

    Migration added for One to One relation between Book and BookDetail.

---
+++ EFCore_DataAccess/Migrations/20250709022638_OneToOneRelationBetweenBookAndBookDetailAdded.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class OneToOneRelationBetweenBookAndBookDetailAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Book_Id",
+                table: "BookDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookDetails_Book_Id",
+                table: "BookDetails",
+                column: "Book_Id",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookDetails_TBL_Books_Book_Id",
+                table: "BookDetails",
+                column: "Book_Id",
+                principalTable: "TBL_Books",
+                principalColumn: "IDBook",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookDetails_TBL_Books_Book_Id",
+                table: "BookDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookDetails_Book_Id",
+                table: "BookDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Book_Id",
+                table: "BookDetails");
+        }
+    }
+}
