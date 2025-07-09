d94eacd88756c0c1c8c71e3bec6e727d82bfd7a8
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-09 08:41:00 +0500

    Migration added for Fluent_Book Table.

---
+++ EFCore_DataAccess/Migrations/20250709033952_AddFluentBookTableToDatabase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFluentBookTableToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBL_FluentBooks",
+                columns: table => new
+                {
+                    IDBook = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Fluent_Book_ISBN = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBL_FluentBooks", x => x.IDBook);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBL_FluentBooks");
+        }
+    }
+}
