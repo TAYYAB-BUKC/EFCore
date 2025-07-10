daa56a90cfa9e4bef94bdd7ada35850f65de99af
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-11 00:53:21 +0500

    Index endpoint implemented in SubCategoryController in Web Project.

---
+++ EFCore_Web/Controllers/SubCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using EFCore_DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore_Web.Controllers
+{
+	public class SubCategoryController : Controller
+	{
+		private readonly ApplicationDbContext _applicationDbContext;
+		public SubCategoryController(ApplicationDbContext applicationDbContext)
+		{
+			_applicationDbContext = applicationDbContext;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var list = await _applicationDbContext.SubCategories.ToListAsync();
+			return View(list);
+		}
+	}
+}
