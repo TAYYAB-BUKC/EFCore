adfffd8efd231bad746fcdf567b7a8df0cc9b97f
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-11 02:03:16 +0500

    Upsert endpoints implemented in PublisherController in WebProject.

--- EFCore_Web/Controllers/PublisherController.cs
+++ EFCore_Web/Controllers/PublisherController.cs
@@ -1,4 +1,5 @@
 ﻿using EFCore_DataAccess.Data;
+using EFCore_Models.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,42 @@ namespace EFCore_Web.Controllers
 		{
 			var list = await _dbContext.Publishers.ToListAsync();
 			return View(list);
+		}
+
+		public async Task<IActionResult> Upsert(int? id)
+		{
+			Publisher publisher = new();
+			if(id > 0)
+			{
+				publisher = await _dbContext.Publishers.FindAsync(id);
+				if (publisher is null)
+				{
+					return NotFound();
+				}
+			}
+			return View(publisher);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Upsert(Publisher publisher)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(publisher);
+			}
+
+			if (publisher.Publisher_Id > 0)
+			{
+				_dbContext.Publishers.Update(publisher);
+			}
+			else
+			{
+				await _dbContext.Publishers.AddAsync(publisher);
+			}
+
+			await _dbContext.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
 		}
 	}
 }
