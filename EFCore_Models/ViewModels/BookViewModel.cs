eebce7b2a03f697e93b142f0896a6ec82d6abd9e
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-12 01:38:07 +0500

    Upsert get endpoint implemented in BookController in Web Project.

---
+++ EFCore_Models/ViewModels/BookViewModel.cs
@@ -0,0 +1,11 @@
+﻿using EFCore_Models.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EFCore_Models.ViewModels
+{
+	public class BookViewModel
+	{
+		public Book Book { get; set; }
+		public IEnumerable<SelectListItem> PublisherList { get; set; }
+	}
+}
--- EFCore_Web/Controllers/BookController.cs
+++ EFCore_Web/Controllers/BookController.cs
@@ -1,4 +1,5 @@
 ﻿using EFCore_DataAccess.Data;
+using EFCore_Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,20 @@ namespace EFCore_Web.Controllers
 		{
 			var list = await _dbContext.Books.ToListAsync();
 			return View(list);
+		}
+
+		public async Task<IActionResult> Upsert(int? id)
+		{
+			BookViewModel viewModel = new();
+			if (id > 0)
+			{
+				viewModel.Book = await _dbContext.Books.FindAsync(id);
+				if (viewModel.Book is null)
+				{
+					return NotFound();
+				}
+			}
+			return View(viewModel);
 		}
 	}
 }
