1e1a19c38eeb42a59be835d307303cfb3099cd3e
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-14 02:01:32 +0500

    Revert "Lazy loading configured for EF Core."
    This reverts commit cf442e5453d5aa84fcc6d884697c609c96f0eb95.

--- EFCore_Web/Program.cs
+++ EFCore_Web/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
