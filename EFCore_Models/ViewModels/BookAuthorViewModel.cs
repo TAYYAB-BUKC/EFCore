87d985d54588e9591ee2aef2b76871028aadebff
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-14 00:30:42 +0500

    BookAuthorViewModel added in Models Project.

---
+++ EFCore_Models/ViewModels/BookAuthorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EFCore_Models.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EFCore_Models.ViewModels
+{
+	public class BookAuthorViewModel
+	{
+		public Book Book { get; set; }
+		public BookAuthorMapping BookAuthorMapping { get; set; }
+		public IEnumerable<BookAuthorMapping> BookAuthorList { get; set; }
+		public IEnumerable<SelectListItem> AuthorList { get; set; }
+	}
+}
