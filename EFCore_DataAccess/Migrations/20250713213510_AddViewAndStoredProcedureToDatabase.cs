3ec707ad9d25a3bfcb05006c89b0455f5b5149ca
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-14 02:51:54 +0500

    Migration added for View and Stored Procedure.

---
+++ EFCore_DataAccess/Migrations/20250713213510_AddViewAndStoredProcedureToDatabase.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddViewAndStoredProcedureToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE OR ALTER VIEW GetBookDetails
+                                AS
+                                SELECT B.Title, B.Book_ISBN AS ISBN, B.Price FROM TBL_BOOKS B WITH (NOLOCK)");
+
+			migrationBuilder.Sql(@"CREATE OR ALTER VIEW GetAllBooks
+                                AS
+                                SELECT * FROM TBL_BOOKS B WITH (NOLOCK)");
+
+			migrationBuilder.Sql(@"CREATE OR ALTER PROCEDURE GetBookById
+                                @BookId INT
+                                AS
+                                SELECT * FROM TBL_BOOKS B WITH (NOLOCK)
+                                WHERE 1 = 1 AND B.IDBook = @BookId");
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW GetBookDetails");
+			migrationBuilder.Sql("DROP VIEW GetAllBooks");
+			migrationBuilder.Sql("DROP PROCEDURE GetBookById");
+		}
+    }
+}
